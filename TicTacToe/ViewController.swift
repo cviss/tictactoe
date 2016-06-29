c3efc74a39282241a17a45bcae6ecda4a850581b
Cole Weis <colevisserweis@gmail.com>
2016-06-29 21:45:41 +0200

    Actual Final Pre-Network (I was right)

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -24,21 +24,11 @@ class ViewController: UIViewController {
     var game = Game()
     var oImage = UIImage(named: "o")
     var xImage = UIImage(named: "x")
-    var emptyImage = UIImage(named: "white")
 
     //Initialize Button Objects\\
     @IBOutlet weak var turnImage: UIImageView!
     @IBOutlet weak var winLabel: UILabel!
     @IBOutlet weak var board: UIView!
-    @IBOutlet weak var button0: UIButton!
-    @IBOutlet weak var button1: UIButton!
-    @IBOutlet weak var button2: UIButton!
-    @IBOutlet weak var button3: UIButton!
-    @IBOutlet weak var button4: UIButton!
-    @IBOutlet weak var button5: UIButton!
-    @IBOutlet weak var button6: UIButton!
-    @IBOutlet weak var button7: UIButton!
-    @IBOutlet weak var button8: UIButton!
     
     
     override func viewDidLoad() {
@@ -71,15 +61,14 @@ class ViewController: UIViewController {
     
     //Reset Board and Win Label on Press\\
     @IBAction func resetPressed(_ sender: AnyObject) {
-        button0.setImage(emptyImage, for: [])
-        button1.setImage(emptyImage, for: [])
-        button2.setImage(emptyImage, for: [])
-        button3.setImage(emptyImage, for: [])
-        button4.setImage(emptyImage, for: [])
-        button5.setImage(emptyImage, for: [])
-        button6.setImage(emptyImage, for: [])
-        button7.setImage(emptyImage, for: [])
-        button8.setImage(emptyImage, for: [])
+        
+        for subview in board.subviews {
+            print("hi")
+            if let button = subview as? UIButton {
+                print(button.imageView?.image)
+                button.setImage(nil, for: [])
+            }
+        }
         
         winLabel.text = ""
         game.reset()
